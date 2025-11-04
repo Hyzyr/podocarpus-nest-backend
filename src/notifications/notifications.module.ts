1d925f13667a579ec62d1708eb44bb105863ecdc
Hyzyr <avezberdiyew@gmail.com>
2025-11-04 15:25:58 +0500

    fix: Remove unused GlobalNotificationsService export from NotificationsModule

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -3,11 +3,10 @@ import { NotificationsController } from './notifications.controller';
 import { NotificationsService } from './notifications.service';
 import { DatabaseModule } from 'src/shared/database/database.module';
 import { GlobalNotificationsModule } from 'src/global-notifications/global-notifications.module';
-import { GlobalNotificationsService } from 'src/global-notifications/global-notifications.service';
 
 @Module({
   imports: [DatabaseModule, GlobalNotificationsModule],
-  exports: [NotificationsService, GlobalNotificationsService],
+  exports: [NotificationsService, GlobalNotificationsModule],
   controllers: [NotificationsController],
   providers: [NotificationsService],
 })
